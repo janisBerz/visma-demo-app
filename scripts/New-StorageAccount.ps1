3fdcf43f5f195cce6f5f3ee6eb9a48a108eb5057
janisBerz <janis.berzins@hmcts.net>
2020-02-03 23:17:24 +0200

    Updated script

--- scripts/New-StorageAccount.ps1
+++ scripts/New-StorageAccount.ps1
@@ -1,11 +1,9 @@
 # $stagingResourceGroupName = 'visma-demo-app-stg'
 # $stagingStorageAccountName = 'vismademoappstgjb'
+$existingStorageAccount = Get-AzStorageAccount -ResourceGroupName $stagingResourceGroupName -Name $stagingStorageAccountName
 
-if (!(Get-AzStorageAccount -ResourceGroupName $stagingResourceGroupName -Name $stagingStorageAccountName)) {
+if (!($existingStorageAccount)) {
     New-AzResourceGroup -Name $stagingResourceGroupName -Location 'West Europe'
     New-AzStorageAccount -ResourceGroupName $stagingResourceGroupName -Name $stagingStorageAccountName -SkuName Standard_LRS -Location 'West Europe' -Kind StorageV2
     New-AzRmStorageContainer -ResourceGroupName $stagingResourceGroupName -StorageAccountName $stagingStorageAccountName -Name 'staging'
 }
-
-
-
