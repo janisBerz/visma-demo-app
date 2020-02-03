f82795501fe09a6b5d42147c7ecd4de735c1e27c
janisBerz <janis.berzins@hmcts.net>
2020-02-03 23:13:00 +0200

    Updatde pipeline

--- scripts/New-StorageAccount.ps1
+++ scripts/New-StorageAccount.ps1
@@ -1,5 +1,5 @@
-$stagingResourceGroupName = 'visma-demo-app-stg'
-$stagingStorageAccountName = 'vismademoappstgjb'
+# $stagingResourceGroupName = 'visma-demo-app-stg'
+# $stagingStorageAccountName = 'vismademoappstgjb'
 
 if (!(Get-AzStorageAccount -ResourceGroupName $stagingResourceGroupName -Name $stagingStorageAccountName)) {
     New-AzResourceGroup -Name $stagingResourceGroupName -Location 'West Europe'
