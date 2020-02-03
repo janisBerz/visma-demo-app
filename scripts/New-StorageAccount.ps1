e229646bd2ea492f4635ca93117bb66650fc17d0
janisBerz <janis.berzins@hmcts.net>
2020-02-03 23:32:02 +0200

    Updated script

--- scripts/New-StorageAccount.ps1
+++ scripts/New-StorageAccount.ps1
@@ -5,7 +5,7 @@ Param (
     )
 $existingStorageAccount = Get-AzStorageAccount -ResourceGroupName $stagingResourceGroupName -Name $stagingStorageAccountName -verbose
 
-if (!($existingStorageAccount)) {
+if ($null -eq $existingStorageAccount) {
     New-AzResourceGroup -Name $stagingResourceGroupName -Location 'West Europe' -verbose
     New-AzStorageAccount -ResourceGroupName $stagingResourceGroupName -Name $stagingStorageAccountName -SkuName Standard_LRS -Location 'West Europe' -Kind StorageV2 -verbose
     New-AzRmStorageContainer -ResourceGroupName $stagingResourceGroupName -StorageAccountName $stagingStorageAccountName -Name 'staging' -verbose
